2fefce41a75659b459f810bddd10b84ae70642ff
chylinski.t <chylinski.t@outlook.com>
2019-11-07 22:29:05 -0500

    created initial variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+//create qeestions display one at a time
+
+//keep count of correct and wrongs
+
+//timer for each question
+
+//if question selected is correct promt "Congrats"
+
+//if time runs out alert and move to next page
+
+//if user hovers on option change class to active
+
+//on final screen show correct answers, incorrects answers and option to restart game (without reloading the page)
+//
+
+var correctGuess = 0;
+var incorrectsGuess = 0;
+var unanswered = 0;
+var timer;
+var question = {
+  firstQ: "Most Wins In The Premier League",
+  secondQ: "Top Scorer Of The Premier League",
+  thirdQ: "Most Defeats In The Premier League",
+  fourthQ: "Most Premier League Apperances"
+};
