15695a74557090a69e5189ada2373983e3a36a05
chylinski.t <chylinski.t@outlook.com>
2019-11-07 23:10:00 -0500

    added my questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,9 +17,48 @@ var correctGuess = 0;
 var incorrectsGuess = 0;
 var unanswered = 0;
 var timer;
-var question = {
-  firstQ: "Most Wins In The Premier League",
-  secondQ: "Top Scorer Of The Premier League",
-  thirdQ: "Most Defeats In The Premier League",
-  fourthQ: "Most Premier League Apperances"
-};
+var gameQuestions = [
+  {
+    question: "Most Wins In The Premier League?",
+    answers: {
+      a: "Liverpool FC",
+      b: "Leeds United",
+      c: "Manchester United",
+      d: "Arsenal FC"
+    },
+    correctAnswer: "c"
+  },
+
+  {
+    question: "Top Scorer Of The Premier League?",
+    answers: {
+      a: "Thierry Henry",
+      b: "Didier Drogba",
+      c: "Alan Shearer",
+      d: "Wayne Rooney"
+    },
+    correctAnswer: "c"
+  },
+
+  {
+    question: "Most Defeats In The Premier League?",
+    answers: {
+      a: "Newcastle United",
+      b: "Sunderland AFC",
+      c: "Blackburn Rovers",
+      d: "Everton FC"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "Most Premier League Apperances?",
+    answers: {
+      a: "Gareth Barry",
+      b: "Ryan Giggs",
+      c: "Frank Lampard",
+      d: "James Milner"
+    }
+  }
+];
+
+$(document).ready(() => {});
